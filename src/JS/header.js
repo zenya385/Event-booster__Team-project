65577327c9f28d5dd9427b43c82959d2f47f96e4
VitaPavlenko <91799017+VitaPavlenko@users.noreply.github.com>
2022-01-20 21:32:56 +0200

    markup 4

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { debounce } from "debounce";
+import { debounce } from 'debounce';
 import fetchImages from './input';
 
 const refs = {
@@ -7,11 +8,11 @@ const refs = {
   searchingInput: document.querySelector('.header__form--input'),
   countryInput: document.querySelector('.header__form--input1'),
 };
-refs.headerForm.addEventListener('input',debounce(asd,500));
+refs.headerForm.addEventListener('input', debounce(asd, 500));
 
 function asd(e) {
   e.preventDefault();
   const searchingInput = refs.searchingInput.value;
   const countryInput = refs.countryInput.value;
-  fetchImages(searchingInput,countryInput);
+  fetchImages(searchingInput, countryInput);
 }
