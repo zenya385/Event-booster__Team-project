7dee792d9b5642e3a53bc1ae3f995a818afee5de
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-01-21 19:37:22 +0200

    push

--- src/JS/header.js
+++ src/JS/header.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { debounce } from 'debounce';
 import fetchImages from './input';
 import templates from '../templates/markup.hbs';
-import modalMarkup from '../templates/modal.hbs'
-
+import modalMarkup from '../templates/modal.hbs';
+const arr = [];
 const refs = {
   headerForm: document.querySelector('.header__form'),
   searchingInput: document.querySelector('.header__form--input'),
@@ -11,57 +11,51 @@ const refs = {
   gallery: document.querySelector('.gallery'),
 };
 
-
 refs.headerForm.addEventListener('input', debounce(asd, 500));
 
 function asd(event) {
   event.preventDefault();
   const searchingInput = refs.searchingInput.value;
-  console.log();
   const countryInput = refs.countryInput.value;
   fetchImages(searchingInput, countryInput).then(({ _embedded }) => {
-    const getDetails = _embedded.events
-      .flatMap(obj => {
-        // console.log(obj.images);
-        console.log(obj.id)
-        return obj.id;
-      })
-      .join(',');
-    //  console.log(getDetails);
-    const markUp = templates(_embedded.events);
-    refs.gallery.insertAdjacentHTML('beforebegin', markUp);
-    console.log(_embedded.events);
-    // console.log({ _embedded,id });
-    // console.log(id);
-    return getDetails;
-  }).then(event =>{
-    
-    // console.log(event[0].id);
-    console.log(event);
-  })
+    function arrr(a) {
+      const y = a.map(names => {
+        console.log(names.images[0].url);
+        // console.log(names);
+        return `
+        <div class='photo-card' data-id='{{id}}'>
+        <img
+          class='img'
+          src='${names.images[0].url}'
+          alt=''
+          loading='lazy'
+        />
+        <div class='info'>
+          <p class='info-item-name'>
+            <b><span>${names.name}</span></b>
+          </p>
+          <p class='info-item-lokal'>
+            <span>${names.dates.start.localDate}</span>
+          </p>
+          <p class='info-item-address'>
+            <b><span></span></b>
+          </p>
+        </div>
+      </div>
+        `;
+
+        // arr.push(names.name);
+        // arr.push(names.url);
+        // arr.push(names.dates.start.localDate);
+        // return arr;
+      });
+      refs.gallery.insertAdjacentHTML('beforebegin', y);
+    }
+    // for (let i = 0; i < names.length; i++) {
+    //   let prop = names[i].url
+    //   console.log(prop.url,"at:",prop.x,prop.y);
+    arrr(_embedded.events);
+    // console.log(arr);
+    // const markUp = templates(arr);
+  });
 }
-
-
-// // console.log(modal)
-// refs.headerForm.addEventListener('input', debounce(asd, 500));
-
-// function asd(event) {
-//   event.preventDefault();
-//   const searchingInput = refs.searchingInput.value;
-//   // console.log();
-//   const countryInput = refs.countryInput.value;
-//   fetchImages(searchingInput, countryInput).then(({ _embedded,id }) => {
-//     const markUp = templates(_embedded.events);
-//     refs.gallery.insertAdjacentHTML('beforebegin', markUp);
-//     // console.log(_embedded.events);
-
-
-//     // return markUp;
-//     return _embedded.events;
-//   }).then(event =>{
-//     // console.log(event);
-//     const modalM = modalMarkup(event)
-//     console.log(modalM)
-
-//   })
-// }
--- src/JS/input.js
+++ src/JS/input.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import templates from '../templates/markup.hbs';
-const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
-const API_KEY = 'mwPGNQc6oAGCcUN76qKb1vw6XCSZXdwt';
+// import templates from '../templates/markup.hbs';
+// const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
 
 // export default async function fetchImages(event, country) {
 //   try {
@@ -17,7 +16,7 @@ const API_KEY = 'mwPGNQc6oAGCcUN76qKb1vw6XCSZXdwt';
 // }
 export default function fetchImages(event, country) {
   const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
-  const API_KEY = 'mwPGNQc6oAGCcUN76qKb1vw6XCSZXdwt';
+  const API_KEY = 'bdHFOjAkpUBvne7gzKAkA6SZNtgLzUV4';
 
   const url = `${BASE_URL}/events.json?keyword=${event}&apikey=${API_KEY}&countryCode=${country}&page=1&size=20`;
   return fetch(url).then(response => response.json());
