74f7d1bfbf4035d66a40d5d4d4f9a2deb8584d5d
Nick Arabadzhi <nikita.arabadji@gmail.com>
2022-01-22 11:26:25 +0200

    upadte

--- src/JS/input.js
+++ src/JS/input.js
@@ -18,6 +18,8 @@ export default function fetchImages(event, country) {
   const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
   const API_KEY = 'bdHFOjAkpUBvne7gzKAkA6SZNtgLzUV4';
 
-  const url = `${BASE_URL}/events.json?keyword=${event}&apikey=${API_KEY}&countryCode=${country}&page=1&size=20`;
+  const url = `${BASE_URL}/events.json?keyword=${event}&apikey=${API_KEY}`;
   return fetch(url).then(response => response.json());
 }
+
+// &countryCode=${country}
