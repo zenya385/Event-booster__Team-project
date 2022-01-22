14c3931cfb672dcef0c97c986c27ca2b2260b0b9
VitaPavlenko <91799017+VitaPavlenko@users.noreply.github.com>
2022-01-22 21:01:44 +0200

    modal

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,14 +1,38 @@
-export default function modal() {
-    const refs = {
-      openModalBtn: document.querySelector('.gallery'),
-      closeModalBtn: document.querySelector('.modal-close-js'),
-      modal: document.querySelector('.modal-js'),
-    };
+// export default function modal() {
+//   const refs = {
+//     openModalBtn: document.querySelector('.gallery'),
+//     closeModalBtn: document.querySelector('.modal-close-js'),
+//     modal: document.querySelector('.modal-js'),
+//   };
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    // refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  };
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// }
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+// console.log(refs.closeModalBtn);
+
+//   // refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', closeModal);
+
+//   // function toggleModal() {
+//   //   refs.modal.classList.remove('is-hidden');
+//   //   console.log('ok');
+//   // }
+
+//   function closeModal() {
+//     refs.modal.classList.add('is-hidden');
+
+//     console.log('xxx');
+//   }
+// })();
