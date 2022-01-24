a754a0b9dd153c562540329e3bbffc9e62e55b24
Nick Arabadzhi <nikita.arabadji@gmail.com>
2022-01-24 17:49:27 +0200

    commit

--- src/JS/header.js
+++ src/JS/header.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { debounce } from 'debounce';
 import fetchImages from './input';
 import fetchModalInfo from './main';
-import countryCode from './fetchCountryCodes';
+import {paginationMarkup} from './pagination';
 
 const refs = {
   headerForm: document.querySelector('.header__form'),
@@ -12,53 +12,29 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   modal: document.querySelector('[data-modal]'),
   selectCountry: document.querySelectorAll('#selectCountries'),
+  pageCount: document.querySelector('.pagination'),
 };
-// console.log(refs.countryInput);
 
-// console.log(refs.selectCountry);
+
 refs.headerForm.addEventListener('input', debounce(onInput, 500));
 function onInput(event) {
   event.preventDefault();
   const searchingInput = refs.searchingInput.value;
   const countryInput = refs.countryInput.value;
-  // console.log(countryInput);
-  // countryCode(countryInput).then(({ _embedded}) => {
-  //   console.log(_embedded);
-  //   console.log( _embedded.venues)
-  //   function renderMarkupFilter(events) {
-  //     const markup = events.map(
-  //       event=>{
-  //         return `
-  //       <div class='photo-card' data-div='event' data-id='${event.id}'>
-  //       <img
-  //         class='img'
-  //         src=''
-  //         alt=''
-  //         loading='lazy'
-  //       />
-  //       <div class='info'>
-  //         <p class='info-item-name'>
-  //           <b><span>${event.name}</span></b>
-  //         </p>
-  //         <p class='info-item-lokal'>
-  //           <span></span>
-  //         </p>
-  //         <p class='info-item-address'>
-  //           <b><span></span></b>
-  //         </p>
-  //       </div>
-  //     </div>
-  //       `;
-  //       })
-  //       .join('');
-  //       refs.gallery.innerHTML = markup;
-  //   }
-  //   renderMarkupFilter(_embedded.venues)
-  // });
-  fetchImages(searchingInput, countryInput)
-    .then(({ _embedded }) => {
-      console.log(_embedded);
-      // console.log(_embedded.events[19]._embedded.venues[0].country.countryCode);
+  let page =0;
+  fetchImages(searchingInput, countryInput,page)
+    .then(({_embedded,page}) => {
+      const optionPagination = {
+        showStart : false,
+        showEnd:false,
+        baseTag:'a',
+        link:'https://app.ticketmaster.com/discovery/v2//events.json?apikey=bdHFOjAkpUBvne7gzKAkA6SZNtgLzUV4&page=',
+        baseClass : 'pageCount',
+        query: `countryCode=${countryInput}&keyword=${searchingInput}`
+      }
+ const renderPageMarkup = paginationMarkup(page.totalPages,page.number+1,optionPagination);
+ refs.pageCount.innerHTML = renderPageMarkup;
+      // console.log(page);
       function renderMarkupCards(events) {
         const markup = events
           .map(event => {
@@ -87,9 +63,21 @@ function onInput(event) {
           .join('');
         refs.gallery.innerHTML = markup;
       }
+      
       renderMarkupCards(_embedded.events);
+      refs.pageCount.addEventListener('click',(e)=>{
+        if (e.target.nodeName !== 'A') return;
+       
+      const data = e.target.href;
+      console.log(data);
+      fetch(data).then(response=>response.json()).then(({_embedded})=>{
+        e.preventDefault();
+        renderMarkupCards(_embedded.events);
+      });
+       })
     })
     .catch(console.log);
+    
 }
 document.addEventListener('click', getData);
 
@@ -131,3 +119,4 @@ function getData(e) {
       .catch(console.log('erorr'));
   }
 }
+
--- src/JS/input.js
+++ src/JS/input.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-export default function fetchImages(event, country) {
+// import axios from 'axios';
+export default function fetchImages(event, country,page) {
   const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
   const API_KEY = 'bdHFOjAkpUBvne7gzKAkA6SZNtgLzUV4';
-
-  const url = `${BASE_URL}/events.json?keyword=${event}&apikey=${API_KEY}&countryCode=${country}`;
+  const url = `${BASE_URL}/events.json?keyword=${event}&apikey=${API_KEY}&countryCode=${country}&page=${page}`;
   return fetch(url).then(response => response.json());
 }
 
-//
+
