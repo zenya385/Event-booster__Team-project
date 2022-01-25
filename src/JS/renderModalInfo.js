6ceaf590e7518ceac9bce479993d4ce7a7005438
Nick Arabadzhi <nikita.arabadji@gmail.com>
2022-01-25 13:34:16 +0200

    up

--- src/JS/renderModalInfo.js
+++ src/JS/renderModalInfo.js
@@ -1,7 +1,8 @@
 export default function renderModalInfo(name) {
   const renderMarkup = `
-<div class=" is-hidden modal-js">
-<button data-modal-close class="">X</button>
+<div class="is-hidden modal-js">
+<button data-modal-close class="btn-modal-close">X</button>
+<div class="modal-description">
 <h2>INFO</h2>
 <p>${name.info}</p>
 <h2>WHEN</h2>
@@ -13,9 +14,8 @@ export default function renderModalInfo(name) {
 <h2>PRICES</h2>
 <p><span>|||||</span> STANDART ${name.priceRanges[0].min} - ${name.priceRanges[0].max} ${name.priceRanges[0].currency}</p>
 <button>BUY TICKET</button>
-<p></p>
 <button>BUY TICKET</button>
-<button>MORE FROM THIS AUTHOR</button></div>
+<button>MORE FROM THIS AUTHOR</button></div></div>
 `;
 
   return renderMarkup;
