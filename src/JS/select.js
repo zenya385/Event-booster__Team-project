dfafa3c929db191a73468529dce075224c9d82bc
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-01-26 20:18:22 +0200

    push

--- src/JS/select.js
+++ src/JS/select.js
@@ -1,38 +1,12 @@
-import countryCode from './fetchCountryCodes';
 import countries from '../countries.json';
-import item from '../newCountry.json';
-import renderMarkupCards from './renderMarkupCards';
+const selectCountry = document.querySelector('#selectCountries');
 
-const refs = {
-  headerForm: document.querySelector('.header__form'),
-  searchingInput: document.querySelector('.header__form--input'),
-  countryInput: document.querySelector('.header__form--input1'),
-  gallery: document.querySelector('.gallery'),
-  selectCountry: document.querySelectorAll('#selectCountries'),
-  pageCount: document.querySelector('.pagination'),
-};
-
-import Choices from 'choices.js';
-const element = document.querySelector('#selectCountries');
-console.log(element.value);
-countryCode(element.value).then(response => {
-  refs.gallery.innerHTML = renderMarkupCards(response._embedded.events);
-});
-const choices = new Choices(element, {
-  choices: countries,
-  items: countries,
-});
-
-//
-// countryCode
-// country
-// const data = countries
-//   .map(countries => {
-//     console.log(countries);
-//     `<option class="input__option" value="${countries.countryCode[0]}">${countries.country}</option>`;
-//     return `<option class="input__option" value="${countries.countryCode}">${countries.country}</option>`;
-//     // console.log(country)
-//   })
-//   .join('');
-
-// element.insertAdjacentHTML('beforeend', data);
+const data = countries
+  .map(countries => {
+    console.log(countries.countryCode);
+    `<option class="input__option" value="${countries.countryCode[0]}">${countries.country}</option>`;
+    return `<option class="input__option" value="${countries.countryCode}">${countries.country}</option>`;
+    // console.log(country)
+  })
+  .join('');
+selectCountry.insertAdjacentHTML('beforeend', data);
